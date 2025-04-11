208448e2ffda3eb5da733ea6ff2ac44a67dcac0f
m0neZ <ramonkattan@gmail.com>
2025-04-11 15:21:30 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
@@ -9,4 +7,4 @@ module.exports = {
     extend: {}
   },
   plugins: []
-}
+};
